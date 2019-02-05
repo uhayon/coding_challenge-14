734cd3219fd5662d66cb2e660faa11531ff7748e
Uriel Hayon <uhayon@Mac-mini-de-Uriel.local>
2019-02-05 18:18:29 -0300

    Start new game

--- main.js
+++ main.js
@@ -6,8 +6,23 @@
       this.board = new Board(cards);
       this.moves = new Moves();
       this.elapsedTime = new ElapsedTime();
+      this.matches = {
+        objective: cards / 2,
+        done: 0
+      }
 
-      // this.elapsedTime.startTimer();
+      this.elapsedTime.startTimer();
+    }
+
+    endGame() {
+      this.elapsedTime.stopTimer();
+    }
+
+    addMatch() {
+      this.matchedCards++;
+      if (this.matchedCards === this.matchesToWin) {
+        this.elapsedTime.stopTimer();
+      }
     }
   }
 
@@ -16,22 +31,29 @@
       this.container = document.getElementById('board');
       this.cards = cards.map(card => {
         const cardInstance = new Card(card);
-        cardInstance.htmlElement.addEventListener('click', this.onCardClick.bind(this, cardInstance));
 
         return cardInstance;
       });
+      this.matchedCards = 0;
+      this.matchesToWin = cards / 2;
+
+      this.pickedCard = null;
 
       this.shuffleCards();
+      this.clearBoard();
       this.placeCards();
-    }
-
-    onCardClick(cardInstance) {
-      cardInstance.htmlElement.classList.toggle('face-up');
-      cardInstance.htmlElement.classList.toggle('face-down');
     }
 
     shuffleCards() {
       this.cards.sort(() => Math.random() - 0.5);
+    }
+
+    clearBoard() {
+      const board = document.getElementById('board');
+      
+      for (let i = board.children.length - 1; i >= 0; i--) {
+        board.removeChild(board.children[i]);
+      }  
     }
 
     placeCards() {
@@ -44,10 +66,42 @@
   class Card {
     constructor(cardContent) {
       this.content = cardContent;
-      this.htmlElement = this.buildHtmlCard()
+      this.htmlElement = this.buildHtmlCard();
+      this.matched = false;
     }
 
-    onClick() {}
+    onClick() {
+      if (this.matched) {
+        return;
+      }
+
+      this.htmlElement.classList.toggle('face-up');
+      this.htmlElement.classList.toggle('face-down');
+      if (game.board.pickedCard === null) {
+        game.board.pickedCard = this;
+      } else {
+        const firstCard = game.board.pickedCard;
+        game.board.pickedCard = null;
+        if (firstCard.content === this.content) {
+          firstCard.setMatched();
+          this.setMatched();
+        } else {
+          setTimeout(function() {
+            firstCard.htmlElement.classList.toggle('face-up');
+            firstCard.htmlElement.classList.toggle('face-down');
+            this.htmlElement.classList.toggle('face-up');
+            this.htmlElement.classList.toggle('face-down');
+          }.bind(this), 500);
+        }
+
+        game.moves.add();
+      }
+    }
+
+    setMatched() {
+      this.matched = true;
+      this.htmlElement.classList.add('matched');
+    }
 
     buildHtmlCard() {
       const cardContainer = document.createElement('div');
@@ -65,6 +119,8 @@
       cardContainer.appendChild(cardFrontFace);
       cardContainer.appendChild(cardBackFace);
 
+      cardContainer.addEventListener('click', this.onClick.bind(this));
+
       return cardContainer;
     }
   }
@@ -73,10 +129,13 @@
     constructor() {
       this.count = 0;
       this.label = document.getElementById('moves');
+
+      this.refreshLabel();
     }
 
-    addMove() {
+    add() {
       this.count++;
+      this.refreshLabel();
     }
 
     refreshLabel() {
@@ -89,6 +148,8 @@
       this.time = 0;
       this.label = document.getElementById('time-elapsed'),
       this.interval = null;
+
+      this.refreshLabel();
     }
 
     startTimer () {
@@ -103,9 +164,28 @@
     }
 
     refreshLabel() {
-      this.label.innerText = this.time;
+      this.label.innerText = this.time.toString().toHHMMSS();
     }
   }
 
-  window.game = new Game(cards.concat(cards));
+  String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10);
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+  }
+
+  startNewGame = function() {
+    if (window.game) {
+      window.game.endGame();
+    }
+    window.game = new Game(cards.concat(cards));
+  }
+
+  startNewGame();
 })();
